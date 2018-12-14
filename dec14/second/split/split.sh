acd9224422ae2e45cf7369a5d7fbaf52ed27dc5b
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-12-14 23:08:17 +0900

    Update map

--- dec14/second/split/split.sh
+++ dec14/second/split/split.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-rosbag filter target.bag target_before.bag "t.secs < 1544790057"
+rosbag filter target.bag target_before.bag "t.secs < 1544790051"
 rosbag filter target.bag target_after.bag "t.secs > 1544790061"
